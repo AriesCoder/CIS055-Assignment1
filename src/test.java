fd544c31fa1a36ddd70dbb45419a88d42ced9d86
Aries Lam <ms.tranglam@gmail.com>
2021-09-16 14:47:39 -0700

    changed explanation, delete algorithm, and code description. Added insert algorithm.

--- src/test.java
+++ src/test.java
@@ -2,7 +2,11 @@ public class test {
     public static class Main {
 
         public static void main(String[] args) {
-
+            /*
+            Assume that the holes are in the middle of 2 used elements
+        These codes are for the case that we have a hardcoded array.
+        And, the currentSize variable is affected by the holes if it is got by appending elements.
+         */
             // create an array with holes
             int arr[] = new int[10];
             arr[0] = 1;
@@ -23,13 +27,8 @@ public class test {
             displayNumbers(arr, currentSize(arr));// print out the array to see the update
 
             printLine();
-            //invoke deleteMove function
-            System.out.println("Delete Move: " + deleteMove(10,arr));
-            displayNumbers(arr, currentSize(arr));
-
-            printLine();
             //invoke deleteShift function
-            System.out.println("Delete Shift: " + deleteShift(2, arr));
+            System.out.println("Delete: " + delete(2, arr));
             displayNumbers(arr, currentSize(arr));
 
             printLine();
@@ -37,11 +36,23 @@ public class test {
             System.out.println("Insert Front: " + insertFront(20, arr));
             displayNumbers(arr, currentSize(arr));
 
+            printLine();
+            //invoke InsertFront function
+            System.out.println("Insert Front No Shift: " + insertFrontNoShift(30, arr));
+            displayNumbers(arr, currentSize(arr));
+
         }
-        // To determine the currentSize of used elements in holes version, we create a loop going from the end of the array back to the beginning.
+
+
+        /* If the holes are not counted for currentSize variable,
+           we can create a loop go backward to find currentsize.
+           In this case, the currentSize is a method and the performance is O(n) if the array is not full
+         */
         public static int currentSize(int[] array) {
-            //write a loop that an index goes backward in the array
+
+            //a loop that an index goes backward in the array
             for (int n = array.length -1; n >= 0 ;n--){
+
                 //return currentSize when n meets the first used element
                 if (array[n]!=0){
                     return n+1;
@@ -50,11 +61,17 @@ public class test {
             //if there is no element in an array, return 0
             return 0;
         }
-        // write a function to get an index of an element
+
+
+        /* a search function to get an index of an element.
+            Once we found currentsize, the search algorithm in holes version is the same as no hole.
+            the worst case is O(n)
+         */
         public static int linearSearch(int element, int[] array) {
             int currentSize = currentSize(array);
 
-            // let n be an index in the array. Based on currentSize, n goes from index 0 and stop at the last used element to find an element
+            /* let n be an index in the array.
+             Based on currentSize, n goes from index 0 and stop at the last used element to find an element. */
             for (int n = 0; n < currentSize; n++) {
                 // if there is a matching element in the array, return the index of that element.
                 if (array[n] == element) {
@@ -63,54 +80,58 @@ public class test {
             }
             // if there is no matching element, return -1
             return -1;
-
         }
 
-        // create a method to update element. Using the search algorithm to find the old value that we want to update
+
+
+        /*
+        Update algorithm.
+        this algorithm is based on the search algorithm to find the index of an element to update.
+        it does not have any change in the holes version.
+        The worst case is O(n).
+         */
         public static boolean update(int oldValue, int newValue, int[] array) {
-            int searchIndex = linearSearch(oldValue, array);
+
+            int searchIndex = linearSearch(oldValue, array);//find an index of the element to update.
 
             if (searchIndex == -1)
                 return false; // Element to update is not found
 
             array[searchIndex] = newValue;
-            return true;//Element to update found and updated by new element
+            return true;//Element to update found and updated by a new element
         }
 
-        //create a method to delete an element by shifting the rest elements to fill that position
-        public static boolean deleteShift(int element, int[] array) {
 
-            //find the index of element to delete
+
+        /*
+        Delete algorithm.
+        Array in Java is reserved size which cannot be changed.
+        So, when an element is deleted, it should be replaced by another element.
+        In holes version, to delete an element and leave "a hole", the element to delete will be replaced by 0.
+        It does not require to move or shift other elements to fill the hole.
+        However, it requires to perform search algorithm to find the element to delete.
+        Therefore, it's Big O is still O(n)
+         */
+        public static boolean delete(int element, int[] array){
             int searchIndex = linearSearch(element, array);
-            int currentSize = currentSize(array);
+            //int currentSize = currentSize(array);
 
-            if (searchIndex == -1)
+            if (searchIndex == -1) {
                 return false; // Element to delete is not found
-
-            //shift an element to replace the element that we want to delete.
-            for (int n = searchIndex; n < currentSize - 1; n++)
-                array[n] = array[n+1];
-
-            array[--currentSize] = -1; // “Remove” last element
+            }else{
+                array[searchIndex] = 0;
+            }
             return true;
         }
 
-        //create a method to delete an element by moving the last element of an array to replace it.
-        public static boolean deleteMove(int element, int[] array) {
 
-            int searchIndex = linearSearch(element, array);//find an index of an element to delete
-            int currentSize = currentSize(array);
 
-            if (searchIndex == -1)
-                return false; // Element to delete is not found
-
-            array[searchIndex] = array[--currentSize];
-            array[currentSize] = -1; // “Remove” last element of array
-
-            return true;
-        }
-
-        //create a method to insert an element to the front of the array
+        /*
+        Insert front and shift the rest elements to the right.
+        Because the holes also have value which is 0, the holes are also shifted when the other elements are shifted.
+        So, this algorithm does not change in holes version once currentSize is found.
+        The worst case is O(n) for the holes version
+        */
         public static boolean insertFront(int element, int[] array) {
 
             int currentSize = currentSize(array);
@@ -124,6 +145,28 @@ public class test {
             currentSize++;// current size increase
             return true;
         }
+
+        /*
+        Because the holes are between 2 elements so the first index and last index in array are used elements
+        In the insertFrontNoShift algorithm, the first element is moved to the last.
+        Then the element to insert is inserted.
+        Therefore, the insert algorithm in holes version does not change.
+        To insert an element, it requires to check if the array is full or not which needs currentSize to compare.
+        So, it's big O is O(n)
+         */
+        public static boolean insertFrontNoShift(int element, int[]array)
+        {
+            int currentSize = currentSize(array);
+
+            if (currentSize == array.length)
+                return false; // Array is at maximum capacity
+
+            array[currentSize++] = array[0];
+            array[0] = element;
+            return true;
+        }
+
+
 
         //display an array and a current size of array
         public static void displayNumbers(int[] arr, int currentSize)
